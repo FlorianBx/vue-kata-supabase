dff5552652e8b8b6f03ff6adb3005e6247116349
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-11-02 21:07:54 +0100

    feat: add tailwind, unplugin router

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="h-screen bg-gray-900 text-gray-200 p-4">
+    <header>
+      <nav class="flex gap-4 h-10">
+        <RouterLink class="text-teal-400 hover:text-yellow-200 font-bold" to="/"
+          >Home</RouterLink
+        >
+        <RouterLink
+          class="text-teal-400 hover:text-yellow-200 font-bold"
+          to="/about"
+          >About</RouterLink
+        >
+      </nav>
+    </header>
+    <main class="mt-10">
+      <RouterView />
+    </main>
+  </div>
+</template>
